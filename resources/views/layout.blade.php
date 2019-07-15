d3a7c524f7b1821d3c39b3c8a20a3956f2c3f236
Daniel <danielac@protonmail.com>
2019-07-14 20:08:24 -0600

    My current progress on a Laravel app made through Laracasts tutorials

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <title></title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css">
+
+</head>
+
+<body>
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+</body>
+
+</html>
---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,42 @@
+@extends('layout')
+
+@section('content')
+
+    <h1 class='title'>Create a New Project</h1>
+
+    <form method="POST" action="/projects">
+        {{ csrf_field() }}
+
+        <div class='field'>
+            <label class="label" for="title">Project Title</label>
+
+            <div class="control">
+                <input type="text" class="input {{ $errors->has('title') ? 'is-danger' : '' }}" name="title" value="{{ old('title') }}">
+            </div>
+        </div>
+
+        <div class="field">
+            <label class="label" for="description">Project Description</label>
+
+            <div class="control">
+                <textarea name="description" class="textarea {{ $errors->has('title') ? 'is-danger' : '' }}">{{ old('description') }}</textarea>
+            </div>
+        <div>
+
+        <div class="field">
+            <div class="control">
+                <button type="submit" class="button is-link">Create Project</button>
+        </div>
+
+        @if ($errors->any())
+            <div class="notification is-danger">
+                <ul>
+                   @foreach ($errors->all() as $error)
+                       <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+    </form>
+
+@endsection
