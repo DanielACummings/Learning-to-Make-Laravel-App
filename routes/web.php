c2e086b9afe5b191ae222be823a64de01b480ef5
Daniel <danielac@protonmail.com>
2019-07-15 20:21:01 -0600

    Add ability to create new tasks in projects'

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,9 @@ Route::get('/', function () {
 
 Route::resource('projects', 'ProjectsController');
 
+Route::post('/projects/{project}/tasks', 'ProjectTasksController@store');
 Route::patch('/tasks/{task}', 'ProjectTasksController@update');
+
 
 /*
 What Route::resource(...) expands to:
